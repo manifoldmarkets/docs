eeaae46d240fe48a09027847970e1983fa314153
Austin Chen <akrolsmir@gmail.com>
2022-03-09 10:59:34 -0800

    Fix editUrl to point to our own repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,16 +26,14 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/manticmarkets/docs/tree/main/',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/manticmarkets/docs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
